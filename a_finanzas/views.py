d4c46aa4b268198abf49c2e37c9a8f0aafbec60b
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2025-04-14 17:06:52 -0300

    Actualiza la vista 'index' para renderizar la plantilla 'index.html' con un contexto que incluye un título. También se añade un favicon en la plantilla base.

--- a_finanzas/views.py
+++ a_finanzas/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the financas index.")
+    title = 'Pira Pire'
+    context = {
+        'title':title
+    }
+    return render(request,'index.html',context)
