843124c772f1f6bac3a5c3c5e2187f922a109177
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2025-04-14 16:51:25 -0300

    INIT

---
+++ a_finanzas/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the financas index.")
